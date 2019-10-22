c1c2ba73a5cfc083863cc57f21764d2ba2f86dcd
devindraizin <devindraizin@knights.ucf.edu>
2019-10-21 21:52:21 -0400

    Updated documentation for network interface

--- src/msgPack/Yam.java
+++ src/msgPack/Yam.java
@@ -4,22 +4,52 @@ package msgPack;
 import org.msgpack.value.Value;
 import org.msgpack.value.ValueFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Yam {
-    public int dest;
-    public int cmd;
-    public List<Value> args;
+public class Yam
+{
+    private int dest;
+    private int cmd;
+    private List<Value> args;
 
-    public Yam(int dest, int cmd, List<Value> args) {
+
+    public Yam(int dest, int cmd, List<Value> args)
+    {
         this.dest = dest;
         this.cmd = cmd;
         this.args = args;
     }
 
-    public Value getMsg() {
-        return ValueFactory.newArray(ValueFactory.newInteger(dest),
-                ValueFactory.newInteger(cmd),
-                ValueFactory.newArray(args));
+    public Yam(int dest, int cmd, Value[] args)
+    {
+        this.dest = dest;
+        this.cmd = cmd;
+        this.args = new ArrayList<>(Arrays.asList(args));
+    }
+
+
+    public int getDest()
+    {
+        return dest;
+    }
+
+    public List<Value> getArgs()
+    {
+        return args;
+    }
+
+    public int getCmd()
+    {
+        return cmd;
+    }
+
+
+    public Value getYam()
+    {
+        return ValueFactory.newArray(ValueFactory.newInteger(this.dest),
+                ValueFactory.newInteger(this.cmd),
+                ValueFactory.newArray(this.args));
     }
 }
--- src/netwerking/Netwerk.java
+++ src/netwerking/Netwerk.java
@@ -20,19 +20,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class Netwerk {
+
+//This class is responsible for creating and managing the TCP interface
+//for all incoming and outgoing commands. We implement a thread safe
+//input and output queue that act as the access point to send and retrieve
+//data sent over the network. All data sent over the network conforms to the
+//message packing protocol we implement in the 'Yam' class. The general
+//format of a 'Yam' message is [int cmd, int dest, Value[] args] where
+//Value[] args is an array of dynamic data types that can be any length.
+
+//We use the singleton design pattern since we only ever want 1 network
+//interface running at anytime. This class also exposes methods for
+//interfacing with the input and output queue for retrieving and sending
+//data.
+
+public class Netwerk
+{
+    public ConcurrentLinkedQueue<Yam> outQueue;
+    public ConcurrentLinkedQueue<Yam> inQueue;
+
     private Socket sock;
     private DataInputStream dis;
     private DataOutputStream dos;
     private MessageUnpacker unpack;
     private MessagePacker pack;
-
-    public ConcurrentLinkedQueue<Yam> outQueue;
-    public ConcurrentLinkedQueue<Yam> inQueue;
-
-    private static Netwerk netwerk;
     private Thread readThread;
     private Thread writeThread;
+
+    private static Netwerk netwerk;
 
     private Netwerk()
     {
@@ -51,12 +66,20 @@ public class Netwerk {
         return netwerk;
     }
 
-    class NetwerkReadThread extends Thread {
+    //Because we want to concurrent read and write functionality from
+    //the network interface, we implement a read and write thread to
+    //wait for incoming and outgoing traffic. For the read thread, the
+    //output stream is passed directly to the message pack library to
+    //create a new 'yam' object which is then added to the input queue
+    class NetwerkReadThread extends Thread
+    {
         @Override
-        public void run() {
+        public void run()
+        {
             Logger.getInstance().logMessage("Starting read thread");
 
-            while (!sock.isClosed()) {
+            while (!sock.isClosed())
+            {
                 try {
                     if(unpack.hasNext()) {
                         ImmutableArrayValue msg = unpack.unpackValue().asArrayValue();
@@ -79,6 +102,13 @@ public class Netwerk {
         }
     }
 
+
+    //Because we want to concurrent read and write functionality from
+    //the network interface, we implement a read and write thread to
+    //wait for incoming and outgoing traffic. For the write thread,
+    //we simply check to see if the output queue contains any 'Yam'
+    //objects and if there are, we pass it to the output stream and
+    //pop that 'Yam' from the output queue.
     class NetwerkWriteThread extends Thread {
         @Override
         public void run() {
@@ -88,7 +118,7 @@ public class Netwerk {
             while(!sock.isClosed()) {
                 if (!outQueue.isEmpty()) {
                     try {
-                        pack.packValue(outQueue.poll().getMsg());
+                        pack.packValue(outQueue.poll().getYam());
                         pack.flush();
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -100,6 +130,13 @@ public class Netwerk {
         }
     }
 
+    //This method must be called before any data can be sent or received.
+    //Handles starting the TCP connection with the specified host on the
+    //specified port. First we check to see if the socket is already open
+    //and if not we begin instantiating a new socket, input/output stream,
+    //and the read/write threads. While the socket maintains a connection
+    //the read/write threads continue running and are killed once the socket
+    //is closed.
     public void connect(String host, int port) {
 
         if(sock != null && !sock.isClosed())
@@ -118,6 +155,9 @@ public class Netwerk {
             unpack = MessagePack.newDefaultUnpacker(dis);
             pack = MessagePack.newDefaultPacker(dos);
 
+            inQueue.clear();
+            outQueue.clear();
+
             readThread = new NetwerkReadThread();
             writeThread = new NetwerkWriteThread();
             readThread.start();
@@ -127,6 +167,13 @@ public class Netwerk {
         }
     }
 
+    //Before the socket is closed we have to close the IO streams.
+    //To do this we explicitly close both the message pack
+    //wrappers for the IO streams as well as calling the
+    //close method directly on the streams in case the wrapper
+    //can't close them implicitly. Once all of the IO streams
+    //have been closed, we can close the socket and finally
+    //kill the read/write threads.
     public void closeConnection()
     {
         if(sock != null && !sock.isClosed())
@@ -146,15 +193,26 @@ public class Netwerk {
                 e.printStackTrace();
             }
         }
+        else
+        {
+            Logger.getInstance().logErrorMessage("Connection is already closed");
+        }
     }
 
+
+
+    //NOT PERMANENT METHOD
     public void test() {
         List<Value> args = new ArrayList<>();
         args.add(ValueFactory.newString("testing lmao"));
         args.add(ValueFactory.newBoolean(false));
         args.add(ValueFactory.newFloat(3.141592651));
+
+        //Yam empty = new Yam(1,2, new Value[] {});
+
         for(int i = 0; i < 10; i++) {
             Yam testYam = new Yam(1, i, args);
+            //this.outQueue.add(empty);
             this.outQueue.add(testYam);
         }
     }
