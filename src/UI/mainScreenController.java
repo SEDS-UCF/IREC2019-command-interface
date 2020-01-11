6c401dff8f5fd1606afeb73748c9cef67e6ec07b
devindraizin <devindraizin@knights.ucf.edu>
2020-01-11 09:07:00 -0500

    Added Status box and support for connection status label

--- src/UI/mainScreenController.java
+++ src/UI/mainScreenController.java
@@ -4,6 +4,32 @@ package UI;
 import UI.LogDisplay.LogWindow;
 import UI.screensFramework.ControlledScreen;
 import UI.screensFramework.ScreensController;
+import javafx.application.Platform;
+import javafx.beans.binding.When;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import netwerking.Netwerk;
+
+
+enum wifiEnum
+{
+    CONNECTED("Connected"),
+    DISCONNECTED("Disconnected");
+
+    private final String status;
+
+    wifiEnum(String status)
+    {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+}
 
 public class mainScreenController implements ControlledScreen
 {
@@ -11,6 +37,11 @@ public class mainScreenController implements ControlledScreen
     private ScreensController myController;
 
     LogWindow logWindow;
+
+    ObjectProperty<wifiEnum> wifiStatus = new SimpleObjectProperty<>(wifiEnum.DISCONNECTED);
+
+    @FXML
+    Label wifiStatusLabel;
 
 
 
@@ -27,8 +58,43 @@ public class mainScreenController implements ControlledScreen
     public void initialize()
     {
         logWindow = new LogWindow();
+        wifiStatusLabel.textProperty().bind(
+                new When(wifiStatus.isEqualTo(wifiEnum.CONNECTED)).then(setLabelConnected()).otherwise(setLabelDisconnected())
+        );
+        startStatusChecker();
         start();
     }
+
+    private String setLabelConnected()
+    {
+        wifiStatusLabel.setStyle("-fx-text-fill: #00b600;");
+
+        return wifiEnum.CONNECTED.toString();
+    }
+
+    private String setLabelDisconnected()
+    {
+        wifiStatusLabel.setStyle("-fx-text-fill: RED;");
+
+        return wifiEnum.DISCONNECTED.toString();
+    }
+
+    private void startStatusChecker()
+    {
+        Thread checker = new Thread(() -> {
+            boolean alive = Netwerk.getInstance().isConnected();
+            if (alive) {
+                Platform.runLater(() -> wifiStatus.set(wifiEnum.CONNECTED));
+            } else {
+                Platform.runLater(() -> wifiStatus.set(wifiEnum.DISCONNECTED));
+            }
+
+        });
+
+        checker.setDaemon(true);
+
+    }
+
 
 
     public void getLogWindow()
@@ -41,10 +107,13 @@ public class mainScreenController implements ControlledScreen
 
     }
 
+
     public void testAction()
     {
         testWindow tester = new testWindow();
         tester.getWindow();
     }
+
+
 
 }
